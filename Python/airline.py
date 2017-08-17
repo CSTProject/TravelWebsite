b41a25eaefdaffa5548da1dacee2d7a3a78954fd
DESKTOP-J111E67\kunwa <kunwar31@gmail.com>
2017-08-17 14:58:58 +0530

    Airline api (to be added in next commit)

---
+++ Python/airline.py
@@ -0,0 +1,4 @@
+import urllib.request, json
+with urllib.request.urlopen("http://maps.googleapis.com/maps/api/geocode/json?address=google") as url:
+    data = json.loads(url.read().decode())
+    print(data)
