5c5621f96b4afec36d3179ae341298b04cd322f3
DESKTOP-J111E67\kunwa <kunwar31@gmail.com>
2017-08-18 11:13:30 +0530

    airline data scrape (initail)

--- Python/airline.py
+++ Python/airline.py
@@ -1,4 +1,37 @@
-import urllib.request, json
-with urllib.request.urlopen("http://maps.googleapis.com/maps/api/geocode/json?address=google") as url:
-    data = json.loads(url.read().decode())
-    print(data)
+import sys
+from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QUrl
+from PyQt4.QtWebKit import QWebPage
+from bs4 import BeautifulSoup
+
+class Client(QWebPage):
+    def __init__(self, url):
+        self.app = QApplication(sys.argv)
+        QWebPage.__init__(self)
+        self.loadFinished.connect(self.on_page_load)
+        self.mainFrame().load(QUrl(url))
+        self.app.exec_()
+
+    def on_page_load(self):
+        self.app.quit()
+origin = 'VIDP'
+dest = 'VOBL'
+url = 'https://uk.flightaware.com/live/findflight?origin=' + origin + '&destination=' + dest
+client_response = Client(url)
+source = client_response.mainFrame().toHtml()
+
+def routes_spider(max_pages):
+    page = 1
+
+    while page <= max_pages:
+
+        plain_text = source
+        soup = BeautifulSoup(plain_text, "lxml")
+        for table in soup.findAll('table', {'id':'Results'}):
+            plain_text = table.text
+            print(plain_text)
+
+
+        page +=1
+
+routes_spider(1)
